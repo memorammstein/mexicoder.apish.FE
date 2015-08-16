525cdd026b135996256f592ee48b93d1c61ececc
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2015-08-16 18:58:00 -0500

    Seed code
    codebase for the apish front end, Made with yeoman’s generator-angular
    + remotion of unnecessary things and added bootstrap and font awesome
    through bower with fixes to get working (current generator-angular has
    some messed up stuff that doesn’t let grunt play nice with said bower
    components if don’t know what you are doing).

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mexicoderapishfeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mexicoderapishfeApp
+ */
+angular.module('mexicoderapishfeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mexicoderapishfeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mexicoderapishfeApp
+ */
+angular.module('mexicoderapishfeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
